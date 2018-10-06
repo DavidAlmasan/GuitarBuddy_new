d76d83e02800932e87b2b2b7d5997f236e1d0760
David Almasan <ada28@cam.ac.uk>
2018-10-06 20:34:23 +0100

    added utilities, started working on create_dataset.py

---
+++ utilities/constants.py
@@ -0,0 +1,27 @@
+def chordToIndex(chord):
+    """Takes <chord> variable and returns an integer"""
+    chordDict = {"A": 0,
+                 "Am": 1,
+
+                 "B": 2,
+                 "Bm": 3,
+
+                 "C": 4,
+                 "Cm": 5,
+
+                 "D": 6,
+                 "Dm": 7,
+
+                 "E": 8,
+                 "Em": 9,
+
+                 "F": 10,
+                 "Fm": 11,
+
+                 "G": 11,
+                 "Gm": 12,}
+
+    return chordDict(chord)
+
+
+                 }
