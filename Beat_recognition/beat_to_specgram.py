cc1e82804a8ceeb8834672d40eb61b8bfd0430e5
DavidAlmasan <davidalmasan@gmail.com>
2018-10-14 22:29:05 +0100

    added beat_to_specgram

--- Beat_recognition/beat_to_specgram.py
+++ Beat_recognition/beat_to_specgram.py
@@ -3,6 +3,25 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../utilities"))
 import wave_reader
+import matplotlib.pyplot as plt
 
-wave_reader.WaveReader("../voice_rec")
 
+class BeatRecognition():
+    def __init__(self):
+        self.pathToBeats = os.path.join(os.path.dirname(__file__), "../voice_recorder_samples/beats")
+        self.soundSamples = os.listdir((self.pathToBeats))
+
+        self.filtering()
+
+    def filtering(self):
+        for sample in self.soundSamples:
+            samplePath = os.path.join(self.pathToBeats, sample)
+            waveFile = wave_reader.WaveReader(samplePath)
+            print(waveFile.frameRate)
+            spectrum, freq, t, image = plt.specgram(waveFile.frames, NFFT= 1024, Fs = waveFile.frameRate, noverlap = 0)
+            plt.show()
+
+
+
+if __name__ == "__main__":
+    BeatRecognition()
