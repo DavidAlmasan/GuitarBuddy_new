d5e55b211f07abb64d60198e2433918b791d4089
DavidAlmasan <davidalmasan@gmail.com>
2018-10-15 11:26:39 +0100

    added fft to create_dataset

--- Training_set/create_dataset.py
+++ Training_set/create_dataset.py
@@ -9,17 +9,21 @@ import sys
 import os
 import numpy as np
 sys.path.append(os.path.join(os.path.dirname(__file__), "../utilities"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../FFT_solution"))
+import fft
 import constants
 import wave_reader
 
 
 class DatasetCreator():
     """Creates a dataset to be fed to the neural network."""
-    def __init__(self):
+    def __init__(self, useFFT = True):
         self.chordFolder = os.path.join(os.path.dirname(__file__), "../Splitter/IndividualChords/")
         self.coordNames = os.listdir(self.chordFolder)
         self.bufferSize = 1024
         self.datasetPath = os.path.join(os.path.dirname(__file__), "./dataset.csv")
+
+        self.useFFT = useFFT
 
         self.createDataset()
 
@@ -37,6 +41,10 @@ class DatasetCreator():
                     sampleIndex = 0
                     while sampleIndex < len(waveFile.frames) - 1024:
                         row = waveFile.frames[sampleIndex:sampleIndex+1024]
+
+                        if self.useFFT:
+                            fftData = fft.FFT(row, samplingRate = waveFile.frameRate)
+                            row = fftData.findFFT()
                         row = np.append(row, constants.chordToIndex(chord))
                         writer.writerow(row)
                         sampleIndex += 1024
